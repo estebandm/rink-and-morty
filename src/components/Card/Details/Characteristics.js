f726894b6385bdf56f72467968b1f894d409844d
Esteban Milicich <edmilicich96@gmail.com>
2021-05-30 19:44:05 -0300

    Modificación de estilos y refactorización del componente Characteristics

--- src/components/Card/Details/Characteristics.js
+++ src/components/Card/Details/Characteristics.js
@@ -15,41 +15,36 @@ import {
 import { Dead, Alive, UnknownStatus } from 'components/core/icons/Status'
 
 export const Characteristics = ({ gender, species, status, fill }) => {
-  const ComponentsGenders = [
-    { Male: <Male fill={fill}/> },
-    { Female: <Female fill={fill}/> },
-    { Genderless: <Genderless fill={fill}/> },
-    { unknown: <Unknown fill={fill}/> }
-  ]
+  const ComponentsGenders = {
+    Male: <Male fill={fill}/>,
+    Female: <Female fill={fill}/>,
+    Genderless: <Genderless fill={fill}/>,
+    unknown: <Unknown fill={fill}/>
+  }
 
-  const ComponentsSpecies = [
-    { Robot: <Robot fill={fill}/> },
-    { Human: <Human fill={fill}/> },
-    { Alien: <Aliens fill={fill}/> },
-    { Animal: <Animal fill={fill}/> },
-    { Humanoid: <Humanoid fill={fill}/> },
-    { Disease: <Disease fill={fill}/> },
-    { MythologicalCreature: <MythologicalCreature fill={fill}/> },
-    { Poopybutthole: <Poopybutthole fill={fill}/> },
-    { Cronenberg: <Cronenberg fill={fill}/> }
-  ]
+  const ComponentsSpecies = {
+    Robot: <Robot fill={fill}/>,
+    Human: <Human fill={fill}/>,
+    Alien: <Aliens fill={fill}/>,
+    Animal: <Animal fill={fill}/>,
+    Humanoid: <Humanoid fill={fill}/>,
+    Disease: <Disease fill={fill}/>,
+    'Mythological Creature': <MythologicalCreature fill={fill}/>,
+    Poopybutthole: <Poopybutthole fill={fill}/>,
+    Cronenberg: <Cronenberg fill={fill}/>
+  }
 
-  const ComponentsStatus = [
-    { Dead: <Dead fill={fill}/> },
-    { Alive: <Alive fill={fill}/> },
-    { unknown: <UnknownStatus fill={fill}/> }
-  ]
+  const ComponentsStatus = {
+    Dead: <Dead fill={fill}/>,
+    Alive: <Alive fill={fill}/>,
+    unknown: <UnknownStatus fill={fill}/>
+  }
+
   return (
     <>
-      {ComponentsGenders.map((componentGender) => (
-        componentGender[gender]
-      ))}
-      {ComponentsSpecies.map(componentSpecie => (
-        componentSpecie[species]
-      ))}
-      {ComponentsStatus.map(componentStatus => (
-        componentStatus[status]
-      ))}
+      {ComponentsGenders[gender]}
+      {ComponentsSpecies[species]}
+      {ComponentsStatus[status]}
     </>
   )
 }
